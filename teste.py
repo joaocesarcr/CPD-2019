b4e34e1d3f279f7205129580ea4d88e88801ed47
joao <joaocesarcrisgolo@gmail.com>
2019-11-24 01:49:14 -0300

    Preparando para separacao das funcoes em arquivos

--- teste.py
+++ teste.py
@@ -95,6 +95,14 @@ def getMatchIDFMH(matchFMH):
 #######################
 # Funcoes que permitem pegar varias partidas
 
+# Recebe um match history e coloca 20 partidas na queue
+# JSON -> Fila
+def put20MatchesFromMH(matchH):
+    for i in range(0,20):
+        match = getMatchFMH(matchH,i)
+        match = getMatchIDFMH(match)
+        matchQueue.put(match)
+
 
 ########
 # MAIN #
@@ -103,21 +111,28 @@ matchQueue = queue.Queue(maxsize=6000)
 matchQueue.put(1773189583)
 #TO-DO: COLOCAR PARTIDAS DE TODOS OS ELOS NA QUEUE
 
-match = getMatchInfo(str(matchQueue.get()))
-summoner = getSummoner(match,8)
-accID = getPlayerAccountId(summoner)
-matchH = getSummonerMatchHistory(accID)
+match = getMatchInfo(str(matchQueue.get())) # Pega partida da fila
+summoner = getSummoner(match,8) # Pega um summoner aleatorio da partida
+accID = getPlayerAccountId(summoner) # Pega o ID do summoner
+matchH = getSummonerMatchHistory(accID) # Pega o historico de partidas do summoner
+put20MatchesFromMH(matchH) # Coloca 20 partidas do jogador na fila
+#print(json.dumps(match, indent=4, sort_keys=False))
+sys.exit()
 
-# Pega 20 partidas de um match history
-for i in range(0,20):
-    match = getMatchFMH(matchH,i)
-    match = getMatchIDFMH(match)
-    matchQueue.put(match)
+
+# Esboco loop
+match = matchQueue.get()
+match = matchQueue.get()
+match = matchQueue.get()
+match = getMatchInfo(str(matchQueue.get())) # Pega partida da fila
+accID = getPlayerAccountId(summoner) # Pega o ID do summoner
+summoner = getSummoner(match,2) # Pega um summoner aleatorio da partida
+matchH = getSummonerMatchHistory(accID) # Pega o historico de partidas do summoner
+put20MatchesFromMH(matchH) # Coloca 20 partidas do jogador na fila
 
 matchQueueSize = matchQueue.qsize()
 for i in range(0, matchQueueSize):
     print(matchQueue.get())
-sys.exit()
 
 # print(json.dumps(matchH["matches"][1], indent=4, sort_keys=False))
 # for player in match["participantIdentities"]:
@@ -129,4 +144,3 @@ sys.exit()
 
 # with open('output.json', 'w') as json_file:
     # json.dump((match, indent=4,sort_keys=True),json_file) 
-
