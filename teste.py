975dad554a37e0b81f316b0a436ae567fc666519
joao <joaocesarcrisgolo@gmail.com>
2019-11-24 00:07:30 -0300

    Funcoes para pegar partidas de um historico de partidas

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import json
+import queue
 import time
 
 #######
@@ -12,7 +13,6 @@ keyFile = open("apiKey.txt", 'r')
 key = keyFile.read() # Le o arquivo
 key = key.rstrip()   # Remove o \n
 api_key = "api_key=" + key # Finaliza a chave que será usada nas requests
-matchID =str(1773189583)
 
 ################
 # TIME CONTROL #
@@ -22,6 +22,7 @@ matchID =str(1773189583)
 
 # Recebe uma funcao e um parametro para ser utilizado nela. Retorna o tempo de decorrimento da funcao e seu retorno
 # str + generic -> int, generic
+# TO-D0 : Transformar parameter em uma lista
 def functionTime(functionName, parameter):
     initTime = time.time()
     retorno = functionName(parameter)
@@ -32,21 +33,29 @@ def functionTime(functionName, parameter):
 # MANIPULACAO COM HTML #
 ########################
 
+# Informa se ocorrou algum erro na request
+# Caso tenha ocorrido, o status_code será maior ou igual a 400
+def requestError(response):
+    if (response.status_code >= 400): return True
+    else: return False
+
 # match v4
 # Recebe o ID de uma partida e retorna um json com todas as informacoes
 # int -> json
-
-def getMatchInfo(matchid):
+def getMatchInfo(matchID):
     URL = "https://br1.api.riotgames.com/lol/match/v4/matches/" + matchID + "?" + api_key
     response = requests.get(URL)
-    return response.json()
+    if requestError(response): return None
+    else: return response.json()
+
 
 # Recebe um account id e retorna seu historico de partidas contendo apenas partidas rankeadas
 # String -> json
 def getSummonerMatchHistory(accountid):
     URL = "https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountid + "?queue=420&" + api_key
     response = requests.get(URL)
-    return response.json()
+    if requestError(response): return None
+    else: return response.json()
 
 #####################################################
 # MANIPULACAO COM OS DADOS RECEBIDOS DE UMA REQUEST #
@@ -57,7 +66,6 @@ def getSummonerID(summoner):
     sumID = str(summoner["summonerId"])
     return sumID
 
-
 # Recebe uma partida e um inteiro e retorna as informacoes do jogador na posicao int
 # int + int -> json
 def getSummoner(match, number):
@@ -66,29 +74,54 @@ def getSummoner(match, number):
     
 # Recebe um sumonner  de uma partida e retorna o seu ID
 # int + int -> int
-
 def getPlayerAccountId(summoner):
     accountID = summoner["player"]["accountId"]
     return accountID 
 
-    
+# Get match from match history
+# Recebe um historico de partidas e uma posicao e retorna a partida a posicao number
+# json + int -> json
+def getMatchFMH(matchHistory, number):
+    return matchHistory["matches"][number]
+
+# Get match ID from a match in match history
+# Recebe uma partida de um historico de partidas e retorna seu ID
+# json -> int 
+def getMatchIDFMH(matchFMH):
+    return matchFMH["gameId"]
+
+#######################
+# FUNCOES PARA O LOOP #
+#######################
+# Funcoes que permitem pegar varias partidas
 
 
-##########
-# TESTES #
-##########
+########
+# MAIN #
+########
+matchQueue = queue.Queue(maxsize=6000)
+matchQueue.put(1773189583)
+#TO-DO: COLOCAR PARTIDAS DE TODOS OS ELOS NA QUEUE
 
-match = getMatchInfo(matchID)
+match = getMatchInfo(str(matchQueue.get()))
 summoner = getSummoner(match,8)
 accID = getPlayerAccountId(summoner)
 matchH = getSummonerMatchHistory(accID)
-for match in matchH["matches"]:
-    print(json.dumps(match, indent=4, sort_keys=False))
+
+# Pega 20 partidas de um match history
+for i in range(0,20):
+    match = getMatchFMH(matchH,i)
+    match = getMatchIDFMH(match)
+    matchQueue.put(match)
+
+matchQueueSize = matchQueue.qsize()
+for i in range(0, matchQueueSize):
+    print(matchQueue.get())
 sys.exit()
 
-print(json.dumps(match, indent=4, sort_keys=False))
-print(json.dumps(match, indent=4, sort_keys=False))
-sumID = getSummonerID(summoner)
+# print(json.dumps(matchH["matches"][1], indent=4, sort_keys=False))
+# for player in match["participantIdentities"]:
+    # print(json.dumps(player, indent=4, sort_keys=False))
 
 ####################
 # SALVA EM ARQUIVO #
